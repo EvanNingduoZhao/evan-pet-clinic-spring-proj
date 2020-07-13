13e67c1f1c6b02e86b130f92ca14e85d9d1eab74
EvanNingduoZhao <ningduozhao@gmail.com>
2020-07-13 02:30:53 -0400

    Initial Project Add

---
+++ src/main/java/evan/springframework/evanpetclinicspringproj/EvanPetClinicSpringProjApplication.java
@@ -0,0 +1,13 @@
+package evan.springframework.evanpetclinicspringproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvanPetClinicSpringProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvanPetClinicSpringProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evan/springframework/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package evan.springframework.evanpetclinicspringproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvanPetClinicSpringProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
