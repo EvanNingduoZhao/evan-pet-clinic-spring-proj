2da829f2cfd20d4ef8b777cb06f4f9292b0d2886
EvanNingduoZhao <ningduozhao@gmail.com>
2020-07-29 15:13:18 -0400

    make multi-module maven build

--- pet-clinic-web/src/main/java/evan/springframework/evenpetclinicspringproj/EvanPetClinicSpringProjApplication.java
+++ pet-clinic-web/src/main/java/evan/springframework/evenpetclinicspringproj/EvanPetClinicSpringProjApplication.java
@@ -1,4 +1,4 @@
-package evan.springframework.evanpetclinicspringproj;
+package evan.springframework.evenpetclinicspringproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pet-clinic-web/src/test/java/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
+++ pet-clinic-web/src/test/java/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
@@ -1,4 +1,4 @@
-package evan.springframework.evanpetclinicspringproj;
+package evanpetclinicspringproj;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
