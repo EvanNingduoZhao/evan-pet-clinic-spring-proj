ff17186dd19691e95a32e7f4502d0c6af1cf91e5
EvanNingduoZhao <ningduozhao@gmail.com>
2020-07-30 01:25:15 -0400

    refactored EvanPetClinicSpringProjApplicationTests

--- pet-clinic-web/src/test/java/evan/springframework/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
+++ pet-clinic-web/src/test/java/evan/springframework/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
@@ -1,9 +1,13 @@
-package evanpetclinicspringproj;
+package evan.springframework.evanpetclinicspringproj;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+
 class EvanPetClinicSpringProjApplicationTests {
 
 	@Test
