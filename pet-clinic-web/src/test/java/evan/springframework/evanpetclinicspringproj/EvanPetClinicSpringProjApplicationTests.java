077e0cbcd6f5ebc1302cc9961470eb51350a984a
EvanNingduoZhao <ningduozhao@gmail.com>
2020-07-30 11:35:57 -0400

    finished multi module maven build (with one warning). Close #1.

--- pet-clinic-web/src/test/java/evan/springframework/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
+++ pet-clinic-web/src/test/java/evan/springframework/evanpetclinicspringproj/EvanPetClinicSpringProjApplicationTests.java
@@ -1,12 +1,13 @@
 package evan.springframework.evanpetclinicspringproj;
 
+import evan.springframework.evenpetclinicspringproj.EvanPetClinicSpringProjApplication;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes= EvanPetClinicSpringProjApplication.class)
 
 class EvanPetClinicSpringProjApplicationTests {
 
