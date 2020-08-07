66356cfeb75f75ba00ba493500ade9f53a4adbc5
EvanNingduoZhao <ningduozhao@gmail.com>
2020-08-07 13:15:09 -0400

    correct error in VetService

--- pet-clinic-data/src/main/java/evan/springframework/evanpetclinicspringproj/services/VetService.java
+++ pet-clinic-data/src/main/java/evan/springframework/evanpetclinicspringproj/services/VetService.java
@@ -1,13 +1,12 @@
 package evan.springframework.evanpetclinicspringproj.services;
 
-import evan.springframework.evanpetclinicspringproj.model.Owner;
 import evan.springframework.evanpetclinicspringproj.model.Vet;
 
 import java.util.Set;
 
-public class VetService {
+public interface VetService {
 
     Vet findById(Long id);
-    Vet save(Owner vet);
+    Vet save(Vet vet);
     Set<Vet> findAll();
 }
