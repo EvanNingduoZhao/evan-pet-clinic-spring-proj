0a11fcb48e7303e12b0655bbaf1e67e8ec530781
EvanNingduoZhao <ningduozhao@gmail.com>
2020-08-09 01:12:41 -0400

    adding vets page and controller. closes #7.

--- pet-clinic-web/src/main/java/evan/springframework/evenpetclinicspringproj/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/evan/springframework/evenpetclinicspringproj/controllers/IndexController.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    // 这里的意思就是，当试图访问url或者url/或者url/index或者url/index.html
+    // 时Spring就会自动执行index这个method，之后这个method是return一个string，叫index
+    // 但是spring会自动到templates这个folder里找一个叫index.html的file，之后chrome里呈现的
+    // 就是这个html的内容
     @RequestMapping({"","/","index","index.html"})
     public String index(){
         return "index";
