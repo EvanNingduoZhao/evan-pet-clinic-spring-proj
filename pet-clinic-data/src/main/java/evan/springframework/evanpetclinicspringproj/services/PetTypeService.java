e4a8029f14bb237804b547e08cff748094d6d8c4
EvanNingduoZhao <ningduozhao@gmail.com>
2020-08-12 00:26:50 -0400

    added PetTypeService and PetTypeMapService. Closes #29

---
+++ pet-clinic-data/src/main/java/evan/springframework/evanpetclinicspringproj/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package evan.springframework.evanpetclinicspringproj.services;
+
+import evan.springframework.evanpetclinicspringproj.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
