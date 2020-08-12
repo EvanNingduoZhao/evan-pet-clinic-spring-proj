3a391a87a1c9cea7ac514c346a9395ff1cddf8d2
EvanNingduoZhao <ningduozhao@gmail.com>
2020-08-12 00:08:58 -0400

    add Speciality class, add Speciality as an attribute to Vet. Closes #27

---
+++ pet-clinic-data/src/main/java/evan/springframework/evanpetclinicspringproj/model/Speciality.java
@@ -0,0 +1,13 @@
+package evan.springframework.evanpetclinicspringproj.model;
+
+public class Speciality extends BaseEntity{
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
