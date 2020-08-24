bfd2a1e01cfa1bd27848b6199de94aa770777ff0
EvanNingduoZhao <ningduozhao@gmail.com>
2020-08-24 01:44:57 -0400

    Added SpecialityService and SpecialityMapService. Closes # 31

---
+++ pet-clinic-data/src/main/java/evan/springframework/evanpetclinicspringproj/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package evan.springframework.evanpetclinicspringproj.services;
+
+import evan.springframework.evanpetclinicspringproj.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long>{
+}
